d3c19c655423f90815af0a2c9b6568e3c833ae20
Anita Tabaczka <tabaczka.anita@gmail.com>
2022-06-19 23:22:44 +0200

    freq labels

--- Equalizer/Source/PluginEditor.cpp
+++ Equalizer/Source/PluginEditor.cpp
@@ -282,9 +282,9 @@ void ResponseCurveComponent::resized(){
     Graphics g(background);
     Array<float> freqs
     {
-        20, 30, 40, 50, 100,
-        200, 300, 400, 500, 1000,
-        2000, 3000, 4000, 5000, 10000,
+        20, 50, 100,
+        200, 500, 1000,
+        2000, 5000, 10000,
         20000
     };
     
@@ -315,6 +315,32 @@ void ResponseCurveComponent::resized(){
         g.setColour(gDb == 0.f ? Colours::pink : Colours::darkgrey);
         g.drawHorizontalLine(y, left, right);
     }
+    
+    g.setColour(Colours::black);
+    const int fontHeight = 10;
+    g.setFont(fontHeight);
+    for( int i = 0; i < freqs.size(); ++i ){
+        auto f = freqs[i];
+        auto x = xs[i];
+        bool addK = false;
+        String str;
+        if ( f > 999.f ){
+            addK = true;
+            f /= 1000.f;
+        }
+        str << f;
+        if( addK )
+            str << "k";
+        str << "Hz";
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setCentre(x, 0);
+        r.setY(1);
+        
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+    }
+    
 }
 
 juce::Rectangle<int> ResponseCurveComponent::getRenderArea(){
